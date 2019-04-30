f6f4f0cdde2454a149486fcfa6d7b84c9a3ad9b5
Stoyan Korudzhiev <stoyan.korudzhiev@DEV-MACBKPRO-STOYANK.local>
2019-04-30 15:07:33 +0300

    Adding a layout for the UI Tests
    - FAB Button with implemented listener leading to another activity
    - On the DetailedActivity screen there will be EditText to be used for the tests purposes

---
+++ app/src/androidTest/java/com/example/testingfundamentals/InstrumentationTest.java
@@ -0,0 +1,7 @@
+package com.example.testingfundamentals;
+
+public class InstrumentationTest {
+
+
+
+}
--- app/src/main/java/com/example/testingfundamentals/MainActivity.java
+++ app/src/main/java/com/example/testingfundamentals/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.testingfundamentals;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    FloatingActionButton mFloatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFloatingActionButton = findViewById(R.id.fab);
+        mFloatingActionButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getApplicationContext(), DetailsActivity.class);
+        startActivity(intent);
     }
 }
