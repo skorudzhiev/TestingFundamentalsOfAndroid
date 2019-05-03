bd7c23783dfec874a7854dcbeaf28e0840984ac6
Stoyan Korudzhiev <stoyan.korudzhiev@DEV-MACBKPRO-STOYANK.local>
2019-05-03 09:45:32 +0300

    Instrumentation test on MainActivity to verify UI elements being showed
    - InstrumentationTest.class was created to elevate the usage of Espresso library
    - Rule annotator was introduced to initiate the Activity which will be used to start the test
    - Within the actual Test annotation is the method which perform click on a FloatingActionButton
    and then ensures that EditText is being shown on the screen

--- app/src/androidTest/java/com/example/testingfundamentals/InstrumentationTest.java
+++ app/src/androidTest/java/com/example/testingfundamentals/InstrumentationTest.java
@@ -1,7 +1,33 @@
 package com.example.testingfundamentals;
 
+import android.support.test.rule.ActivityTestRule;
+import org.junit.Rule;
+import org.junit.Test;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+
 public class InstrumentationTest {
 
+    /**
+     * {@link ActivityTestRule} is a JUnit {@link Rule @Rule} to launch your activity under test.
+     *
+     * <p>
+     * Rules are interceptors which are executed for each test method and are important building
+     * blocks of Junit tests.
+     */
 
+    @Rule
+    public ActivityTestRule<MainActivity> mMainActivityActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void clickFloatingActionButton_opensDetailsActivity() throws Exception {
+        onView(withId(R.id.fab)).perform(click());
+
+        onView(withId(R.id.add_text)).check(matches(isDisplayed()));
+    }
 
 }
