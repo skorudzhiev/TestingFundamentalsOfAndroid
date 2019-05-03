d286c31dfbb5e4a70a8dd1d8848def27ce07f725
Stoyan Korudzhiev <stoyan.korudzhiev@DEV-MACBKPRO-STOYANK.local>
2019-05-03 11:34:09 +0300

    Instrumentation test to type and validate entered text in a ListView
    * Following XML elements has been introduced:
    - ListView
    - Additional button to clear values from the screen
    * The initial test case was refactored to meet the following scenario:
    - Performing auto type in the EditText field
    - Clicks the save button to populate the ListView
    - Matches the populated value on the screen with the inserted input

--- app/src/androidTest/java/com/example/testingfundamentals/InstrumentationTest.java
+++ app/src/androidTest/java/com/example/testingfundamentals/InstrumentationTest.java
@@ -3,11 +3,23 @@ package com.example.testingfundamentals;
 import android.support.test.rule.ActivityTestRule;
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.Map;
+
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.collection.IsMapContaining.hasEntry;
+import static org.hamcrest.core.AllOf.allOf;
 
 public class InstrumentationTest {
 
@@ -24,10 +36,26 @@ public class InstrumentationTest {
             new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void clickFloatingActionButton_opensDetailsActivity() throws Exception {
+    public void openDetailsActivity_MatchingTypedTextOnScreen() throws Exception {
+        String testNote = "Espresso";
+
         onView(withId(R.id.fab)).perform(click());
 
         onView(withId(R.id.add_text)).check(matches(isDisplayed()));
+
+        // Add a text for the note
+        onView(withId(R.id.add_text)).perform(typeText(testNote), closeSoftKeyboard());
+
+        // Save the note
+        onView(withId(R.id.save_text)).perform(click());
+
+        // Should be used to validate that the typed above note is present on the Screen
+        onData(allOf(is(instanceOf(Map.class)), hasEntry(equalTo("Espresso"), is("item: 1"))));
+
+        // Verify note is displayed on Screen
+        onView(withText(testNote)).check(matches(isDisplayed()));
+
+
     }
 
 }
--- app/src/main/java/com/example/testingfundamentals/DetailsActivity.java
+++ app/src/main/java/com/example/testingfundamentals/DetailsActivity.java
@@ -1,14 +1,53 @@
 package com.example.testingfundamentals;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class DetailsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class DetailsActivity extends ListActivity implements View.OnClickListener {
+
+    ArrayList<String> list = new ArrayList<>();
+    ArrayAdapter adapter;
+    EditText addTextEdit;
+    Button saveButton, clearButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        addTextEdit = findViewById(R.id.add_text);
+        saveButton = findViewById(R.id.save_text);
+        clearButton = findViewById(R.id.clear_text);
+
+        adapter = new ArrayAdapter(this, R.layout.text_container, list);
+
+        saveButton.setOnClickListener(this);
+        clearButton.setOnClickListener(this);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.save_text:
+                String text = addTextEdit.getText().toString();
+                list.add(text);
+                addTextEdit.setText("");
+                adapter.notifyDataSetChanged();
+                break;
+            case R.id.clear_text:
+                adapter.clear();
+                break;
+        }
+
+
     }
 }
